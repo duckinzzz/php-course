c100479abfd09b512a0ee2ea807e87bd9fef175d
duckinzzz <yurchenko.ps@ya.ru>
2025-01-13 22:43:07 +1000

    fix

--- src/Repository/ProjectsGroupRepository.php
+++ src/Repository/ProjectsGroupRepository.php
@@ -15,29 +15,4 @@ class ProjectsGroupRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProjectsGroup::class);
     }
-
-    //    /**
-    //     * @return ProjectsGroup[] Returns an array of ProjectsGroup objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?ProjectsGroup
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
